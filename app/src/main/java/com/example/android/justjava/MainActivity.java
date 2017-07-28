213fa788bcb659de738fb3b70b46884a1abd0df9
David Murphy <djmgeneseo@gmail.com>
2017-07-28 02:59:41 -0400

    Add email compatability upon order request, improve aesthetics

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,12 +9,11 @@
 package com.example.android.justjava;
 
 import android.content.Intent;
-import android.icu.text.NumberFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,7 +23,7 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity=2;
+    int quantity=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     /**
      * This method is called when the increment button is clicked.
      */
     public void increment(View view) {
         quantity++;
         displayQuantity(quantity);
-    }
+    } // increment()
 
         /**
          * This method is called when the decrement button is clicked.
@@ -47,32 +45,49 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         if(quantity>0){quantity--; displayQuantity(quantity);}
         else {displayQuantity(quantity);}
-    }
+    } // decrement()
+
 
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
-//        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream);
-//        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-//
-//        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate);
-//        boolean hasChocolate = chocolateCheckBox.isChecked();
-//
-//        EditText nameEditText = (EditText) findViewById(R.id.nameField);
-//        String name = nameEditText.getText().toString();
+    public void checkoutCart(View view) {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
-//        displayMessage(createOrderSummary(calculatePrice(quantity, 5, hasWhippedCream, hasChocolate),
-//                        hasWhippedCream, hasChocolate,  name));
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
 
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("geo:47.6, -122.3"));
+        EditText nameEditText = (EditText) findViewById(R.id.nameField);
+        final String name = nameEditText.getText().toString();
 
-        //Checking to see if there's a component on device that can handle this intent. Does not perform intent if none exist, to prevent app from crashing.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
+        final String priceMessage = (createOrderSummary(calculatePrice(quantity, 5, hasWhippedCream, hasChocolate),
+                        hasWhippedCream, hasChocolate, name));
+
+        displayMessage(priceMessage);
+
+        // Show 'ORDER' button
+        View order = findViewById(R.id.orderButton);
+        order.setVisibility(View.VISIBLE);
+
+        /** Clicking the 'ORDER' button opens email app and sends the order summary
+         *  into the textbox, and adds a subject that includes the user's name
+         * */
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                // intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+                intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+    } // checkoutCart()
+
 
     /**
      * This method calculates the price of a coffee purchase
@@ -86,7 +101,8 @@ public class MainActivity extends AppCompatActivity {
         if (hasWhippedCream) {price++;}
         if (hasChocolate) {price +=2;}
         return price;
-    }
+    } // calculatePrice()
+
 
     /**
      * This method takes in a price and prints a report of the individual's
@@ -96,13 +112,16 @@ public class MainActivity extends AppCompatActivity {
      */
     private String createOrderSummary(int price,  boolean hasWhippedCream, boolean hasChocolate, String name) {
         String orderSummary = "Name: " + name;
-        orderSummary = orderSummary + "\nAdd whipped cream? " + hasWhippedCream;
-        orderSummary = orderSummary + "\nAdd chocolate? " + hasChocolate;
+        orderSummary = orderSummary + "\nAdd whipped cream? ";
+        if (hasWhippedCream) {orderSummary +="Yes!";} else {orderSummary += "No";}
+        orderSummary = orderSummary + "\nAdd chocolate? ";
+        if (hasChocolate) {orderSummary +="Yes!";} else {orderSummary += "No";}
         orderSummary = orderSummary + "\nQuantity: " + quantity;
         orderSummary = orderSummary + "\nTotal: $" + price;
-        orderSummary = orderSummary + "\nThank you!";
+
         return orderSummary;
-    }
+    } // createOrderSummary()
+
 
     /**
      * This method displays the given quantity value on the screen.
@@ -110,13 +129,20 @@ public class MainActivity extends AppCompatActivity {
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
+    } // displayQuantity()
+
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
+        // Show 'Order Summary' View
+        View orderSummary = findViewById(R.id.orderSummary);
+        orderSummary.setVisibility(View.VISIBLE);
+
+        // Show 'Order Summary Text' View
         TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(message);
-    }
+        priceTextView.setVisibility(View.VISIBLE);
+    } // displayMessage()
 }
